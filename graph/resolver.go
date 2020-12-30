e6a49ae2e304b723f98611c417fffdd5f7789e35
ikeyu0806 <ikeyu0806@gmail.com>
2020-12-30 17:10:02 +0900

    implement tutorial todo

--- graph/resolver.go
+++ graph/resolver.go
@@ -3,5 +3,9 @@ package graph
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
-
-type Resolver struct{}
+import (
+	"calendar-backend/graph/model"
+)
+type Resolver struct{
+	todos []*model.Todo
+}
